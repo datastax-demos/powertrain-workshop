195a2127fd9bacbb8b5606549de97a9f747c257d
Peyton Casper <peytoncas@gmail.com>
2017-08-30 02:16:35 -0500

    Fixes for setup

--- .startup/create_application_conf.sh
+++ .startup/create_application_conf.sh
@@ -2,6 +2,6 @@ HOST_IP="$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print 
 
 cd /tmp
 
-cp Powertrain2/conf/application._template.conf application.conf
+cp Powertrain2/conf/application._template.conf Powertrain2/conf/application.conf
 # awk '{gsub("127.0.0.1", '"$HOST_IP"', $0); print}' Powertrain2/conf/application._template.conf > application.conf
 sed -i '/graph_name/c\graph_name="powertrain_graph"' application.conf
