4b1ba505a488119d61dee0ec5002e7a31a8c96b9
Peyton Casper <peytoncas@gmail.com>
2017-08-30 01:36:02 -0500

    Fixes for setup script

--- .startup/start_powertrain.sh
+++ .startup/start_powertrain.sh
@@ -1,5 +1,5 @@
 echo "Starting powertrain"
-cd /tmp/powertrain-workshop/Powertrain2/
+cd /tmp/Powertrain2/
 sed -i '/this.ws = new WebSocket/c\this.ws = new WebSocket("ws://" + "localhost:9000" + "/vehicleStream");' public/game/bkcore/hexgl/VehicleStream.js
 
 sbt playUpdateSecret
