eae5af61c50ab82753bf3307f195c2c663314a74
Peyton Casper <peytoncas@gmail.com>
2017-08-31 15:03:30 -0500

    Added a wait to kafka setup

--- .startup/install_kafka.sh
+++ .startup/install_kafka.sh
@@ -9,4 +9,5 @@ cd kafka_2.10-0.10.0.0
 nohup bin/zookeeper-server-start.sh config/zookeeper.properties 2>&1 1> zookeeper.log &
 nohup bin/kafka-server-start.sh config/server.properties 2>&1 1> kafka.log &
 export KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname='$HOST_IP' -Dcom.sun.management.jmxremote.port=5052"
+sleep 120
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 5 --topic vehicle_events
