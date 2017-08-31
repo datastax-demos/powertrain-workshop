e3caf3f0f04f96da6cb5b5d0343244dd7b390cec
Peyton Casper <peytoncas@gmail.com>
2017-08-31 12:49:25 -0500

    Fix for spark streaming job setup

--- .startup/start_spark_job.sh
+++ .startup/start_spark_job.sh
@@ -1,6 +1,6 @@
-IP="ip route get 1 | awk '{print $NF;exit}'"
+HOST_IP="$(grep 'node0' /etc/hosts | cut -d$'\t' -f 1 | head -n1)"
 echo "Submitting Spark Streaming Job"
-sed -i '/spark.dse_host/c\spark.dse_host $IP' PowertrainStreaming/conf/application.conf
+sed -i '/spark.dse_host/c\spark.dse_host'$'\t'${HOST_IP} /tmp/PowertrainStreaming/conf/application.conf
 cd /tmp/PowertrainStreaming
 sbt package
-nohup dse spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.1  --conf=spark.executor.memory=8g --class powertrain.StreamVehicleData  --properties-file=conf/application.conf target/scala-2.10/streaming-vehicle-app_2.10-1.0-SNAPSHOT.jar 2>&1 1> streaming.log &
+nohup dse spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 --conf=spark.executor.memory=8g --class powertrain.StreamVehicleData  --properties-file=/tmp/PowertrainStreaming/conf/application.conf /tmp/PowertrainStreaming/target/scala-2.10/streaming-vehicle-app_2.10-1.0-SNAPSHOT.jar 2>&1 1> streaming.log &
