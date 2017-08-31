afde38ca97d2256dcb3c87518111c0d35f70bedf
Peyton Casper <peytoncas@gmail.com>
2017-08-30 23:08:06 -0500

    Fix for host ip substition issues

--- .startup/create_application_conf.sh
+++ .startup/create_application_conf.sh
@@ -1,7 +1,6 @@
-HOST_IP="$(grep 'node0_ext' /etc/hosts | cut -d$'\t' -f 1| awk '{ print $1}')"
-
+HOST_IP="$(grep 'node0_ext' /etc/hosts | cut -d$'\t' -f 1)"
 cd /tmp/Powertrain2/conf
 
 cp /tmp/Powertrain2/conf/application._template.conf /tmp/Powertrain2/conf/application.conf
-awk '{gsub("127.0.0.1", '"$HOST_IP"', $0); print}' /tmp/Powertrain2/conf/application._template.conf > application.conf
+sed -i '/dse_graph_host/c\dse_graph_host="'${HOST_IP}'"' application.conf
 sed -i '/graph_name/c\graph_name="powertrain_graph"' application.conf
