d6896b93a2c76e82d4a4cc31b3cddcb6c8088d20
Peyton Casper <peytoncas@gmail.com>
2017-08-30 19:15:38 -0500

    Fixed file pathing of application conf configuration

--- .startup/create_application_conf.sh
+++ .startup/create_application_conf.sh
@@ -1,7 +1,7 @@
 HOST_IP="$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"
 
-cd /tmp
+cd /tmp/Powertrain2/conf
 
-cp Powertrain2/conf/application._template.conf Powertrain2/conf/application.conf
-awk '{gsub("127.0.0.1", '"$HOST_IP"', $0); print}' Powertrain2/conf/application._template.conf > application.conf
+cp /tmp/Powertrain2/conf/application._template.conf /tmp/Powertrain2/conf/application.conf
+awk '{gsub("127.0.0.1", '"$HOST_IP"', $0); print}' /tmp/Powertrain2/conf/application._template.conf > application.conf
 sed -i '/graph_name/c\graph_name="powertrain_graph"' application.conf
