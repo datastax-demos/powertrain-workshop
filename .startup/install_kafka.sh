e4bc00ff3bd239f16f57a306746efcc2c1a35ef7
Peyton Casper <peyton@Peytons-MacBook-Pro.local>
2018-01-28 13:35:39 -0600

    Fixes

--- .startup/install_kafka.sh
+++ .startup/install_kafka.sh
@@ -3,9 +3,9 @@ HOST_IP="$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print 
 #Install kafka
 cd /tmp
 
-wget http://apache.claz.org/kafka/0.10.0.0/kafka_2.10-0.10.0.0.tgz
-tar -xvf kafka_2.10-0.10.0.0.tgz
-cd kafka_2.10-0.10.0.0
+wget http://apache.claz.org/kafka/1.0.0/kafka_2.11-1.0.0.tgz
+tar -xvf kafka_2.11-1.0.0.tgz
+cd kafka_2.11-1.0.0
 nohup bin/zookeeper-server-start.sh config/zookeeper.properties 2>&1 1> zookeeper.log &
 nohup bin/kafka-server-start.sh config/server.properties 2>&1 1> kafka.log &
 export KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname='$HOST_IP' -Dcom.sun.management.jmxremote.port=5052"
--- .startup/install_sbt.sh
+++ .startup/install_sbt.sh
@@ -1,5 +1,5 @@
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-sleep 360
+sleep 60
 sudo apt-get update
 sudo apt-get -y --force-yes install sbt
