04fb8b95d6ebe9762d50f9b8b8a9bbf21f9cbfd2
Peyton Casper <peyton@Peytons-MacBook-Pro.local>
2018-01-29 09:54:49 -0600

    Removed setting auth client id and secret

--- .startup/create_application_conf.sh
+++ .startup/create_application_conf.sh
@@ -9,6 +9,6 @@ sed -i '/github_client_secret/c\github_client_secret="a3056a4e0e66b36aa4436346ba
 
 sed -i '/tokens/c\tokens:["c57b7dfa59d36e8b7edc0134b79f6ee293de6f86"]' application.cfg
 
-cd /tmp/Powertrain2/public/game
+# cd /tmp/Powertrain2/public/game
 
-sed -i '/authorize?client_id/c\<a style="color:#000000" href="https://github.com/login/oauth/authorize?client_id=a81ec76eb967c5dc2d56">' index.html
+# sed -i '/authorize?client_id/c\<a style="color:#000000" href="https://github.com/login/oauth/authorize?client_id=a81ec76eb967c5dc2d56">' index.html
