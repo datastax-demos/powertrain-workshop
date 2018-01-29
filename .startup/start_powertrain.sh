99d0e037c261fc823a6b3022c0fb234f543633e8
Peyton Casper <peyton@Peytons-MacBook-Pro.local>
2018-01-28 18:41:16 -0600

    Switching to node0_ext for contacting vehicle stream

--- .startup/start_powertrain.sh
+++ .startup/start_powertrain.sh
@@ -1,5 +1,5 @@
 echo "Starting powertrain"
-HOST_IP="$(grep 'node0' /etc/hosts | cut -d$'\t' -f 1 | head -n1)"
+HOST_IP="$(grep 'node0_ext' /etc/hosts | cut -d$'\t' -f 1 | head -n1)"
 cd /tmp/Powertrain2/
 sed -i '/this.ws = new WebSocket/c\this.ws = new WebSocket("ws://" + "'${HOST_IP}':9000" + "/vehicleStream");' public/game/bkcore/hexgl/VehicleStream.js
 
