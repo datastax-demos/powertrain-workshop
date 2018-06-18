c01aca7115508d04a48e500305179230dd0772aa
phact <you@example.com>
2018-06-18 18:55:40 +0000

    sbt fixes

--- .startup/start_spark_job.sh
+++ .startup/start_spark_job.sh
@@ -18,9 +18,19 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     IP=localhost
 fi
 
-
 echo "Submitting Spark Streaming Job"
 sedi 's/spark.dse_host.*/spark.dse_host    '${IP}'/' /tmp/PowertrainStreaming/conf/application.conf
+
 cd /tmp/PowertrainStreaming
+
+if [[ $HOSTNAME == "node"* ]] ; then
+su ds_user <<'EOF'
 sbt package
+EOF
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+sbt package
+fi
+
 nohup dse spark-submit --packages org.apache.spark:spark-streaming-kafka-0-10_2.11:2.0.2 --conf=spark.executor.memory=3g --class powertrain.StreamVehicleData --properties-file=/tmp/PowertrainStreaming/conf/application.conf /tmp/PowertrainStreaming/target/scala-2.11/streaming-vehicle-app_2.11-1.0-SNAPSHOT.jar &
