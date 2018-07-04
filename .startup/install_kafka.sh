aada9f6ff72b02bb15ad6eb16621baada7f5ea35
Peyton Casper <peytoncas@gmail.com>
2018-07-03 20:30:31 -0400

    Update install_kafka.sh
    Adjusting the partitions lower

--- .startup/install_kafka.sh
+++ .startup/install_kafka.sh
@@ -23,4 +23,4 @@ nohup bin/zookeeper-server-start.sh config/zookeeper.properties 2>&1 1> zookeepe
 nohup bin/kafka-server-start.sh config/server.properties 2>&1 1> kafka.log &
 export KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname='$IP' -Dcom.sun.management.jmxremote.port=5052"
 sleep 120
-bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 5 --topic vehicle_events
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic vehicle_events
